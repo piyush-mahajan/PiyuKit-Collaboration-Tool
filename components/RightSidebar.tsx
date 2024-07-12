e4f46f8fdfd30bef1397eb0a3fcc27a7ddf3e987
Piyush Mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-07-13 00:35:02 +0530

    Update RightSidebar.tsx

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -7,6 +7,9 @@ import Text from "./settings/Text";
 import Color from "./settings/Color";
 import Export from "./settings/Export";
 import Dimensions from "./settings/Dimensions";
+import Link from "next/link";
+import { exportToPdf } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 const RightSidebar = ({
   elementAttributes,
@@ -32,48 +35,74 @@ const RightSidebar = ({
       syncShapeInStorage,
     });
   };
-  
+
   // memoize the content of the right sidebar to avoid re-rendering on every mouse actions
   const memoizedContent = useMemo(
     () => (
-      <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-w-[227px] sticky right-0 h-full max-sm:hidden select-none">
-        <h3 className=" px-5 pt-4 text-xs uppercase">Design</h3>
-        <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">
-          Make changes to canvas as you like
-        </span>
-
+      <section className='sticky right-0 flex h-full min-w-[227px] select-none flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 max-sm:hidden'>
+        {/* <span className='mt-3 border-b border-primary-grey-200 px-5 pb-4 text-xs text-primary-grey-300'>
+          Created by {""}
+          <Link
+            href='https://your-portfolio-url.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <strong>PIYUSH MAHAJAN</strong>
+          </Link>
+        </span>{" "} */}
+        <h3 className=' px-5 pt-4 text-xs uppercase'>Design Here</h3>
         <Dimensions
           isEditingRef={isEditingRef}
           width={elementAttributes.width}
           height={elementAttributes.height}
           handleInputChange={handleInputChange}
         />
-
         <Text
           fontFamily={elementAttributes.fontFamily}
           fontSize={elementAttributes.fontSize}
           fontWeight={elementAttributes.fontWeight}
           handleInputChange={handleInputChange}
- 
         />
-
         <Color
           inputRef={colorInputRef}
           attribute={elementAttributes.fill}
-          placeholder="color"
-          attributeType="fill"
+          placeholder='color'
+          attributeType='fill'
           handleInputChange={handleInputChange}
         />
-
         <Color
           inputRef={strokeInputRef}
           attribute={elementAttributes.stroke}
-          placeholder="stroke"
-          attributeType="stroke"
+          placeholder='stroke'
+          attributeType='stroke'
           handleInputChange={handleInputChange}
         />
-
         <Export />
+        <div className='flex flex-col gap-3 px-5 py-3'>
+          <h3 className='text-[10px] uppercase'>Build and Designed By </h3>
+          <Link
+            href='https://piyushmahajan.vercel.app/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <Button
+              variant='outline'
+              className='w-full border border-primary-grey-100 hover:bg-primary-green hover:text-primary-black'
+            >
+              Piyush Mahajan
+            </Button>
+          </Link>
+        </div>
+        {/* <span className='mt-10 border-b border-primary-grey-200 px-5 pb-4 text-xs text-primary-grey-300'>
+          Created by {""}
+          <Link
+            href='https://your-portfolio-url.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <strong>PIYUSH MAHAJAN</strong>
+          </Link>
+        </span>{" "} */}
       </section>
     ),
     [elementAttributes]
